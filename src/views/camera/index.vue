3521cc610d0659ad4a49f0b182f85f9b86d68c8c
minicherry <cherrychengyufen@163.com>
2020-04-25 21:31:07 +0800

    开发

--- src/views/camera/index.vue
+++ src/views/camera/index.vue
@@ -48,6 +48,7 @@ export default {
         480)
       // 转base64格式、图片格式转换、图片质量压缩
       let imgBase64 = this.$refs['canvas'].toDataURL('image/jpeg', 0.7)
+
       // 由字节转换为KB判断大小
       let str = imgBase64.replace('data:image/jpeg;base64,', '')
       let strLength = str.length
@@ -57,12 +58,25 @@ export default {
       let size = (fileLength / 1024).toFixed(2)
       console.log(size)
       // 上传拍照信息 调用接口上传图片 .........
+      let file = this.imgSrc; // 把整个base64给file
+      // let type = "image/jpeg";
+      // 定义图片类型（canvas转的图片一般都是png，也可以指定其他类型）
+      let time = (new Date()).valueOf();//生成时间戳
+      let name = time + ".jpg";
+      // 定义文件名字（例如：abc.png ， cover.png）
+      let conversions = this.dataURLtoFile(file, name); // 调用base64转图片方法
+      let parms = new FormData();
+      parms.append('file', conversions);
+
       // 保存到本地
       let ADOM = document.createElement('a')
-      ADOM.href = this.headImgSrc
+      ADOM.href = imgBase64
       ADOM.download = new Date().getTime() + '.jpeg'
       ADOM.click()
     },
+
+    // 上传图片
+    upLoadImage () { },
     // 关闭摄像头
     closeCamera () {
       if (!this.$refs['video'].srcObject) return
